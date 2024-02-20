6ca10b4c6b84b56cdc5c1cfd3b7e68bf0990bd91
Mark Wardle <mark@wardle.org>
2024-02-20 15:16:53 +0000

    Update test to use latest dmd datafile

--- test/com/eldrix/codelists/core_test.clj
+++ test/com/eldrix/codelists/core_test.clj
@@ -25,10 +25,9 @@
 
 (comment
   (def hermes (hermes/open "../hermes/snomed.db"))
-  (def dmd (dmd/open-store "../dmd/dmd-2022-05-09.db"))
+  (def dmd (dmd/open-store "../dmd/dmd-2024-01-29.db"))
   (def env {:com.eldrix/hermes hermes :com.eldrix/dmd dmd})
   (defn ps [id] (vector id (:term (hermes/preferred-synonym hermes id "en-GB"))))
-
   (map ps (cl/realize-concepts env {:icd10 ["B20.*", "B21.*", "B22.*", "B24.*", "F02.4" "O98.7" "Z21.*", "R75"]}))
   (cl/to-icd10 env (cl/realize-concepts env {:ecl "<24700007"})))
 (cl/parse-json "[{\"ecl\": \"<<24700007\"}] ")
